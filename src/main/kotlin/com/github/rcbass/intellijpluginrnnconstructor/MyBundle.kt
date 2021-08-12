b0e7dd6162b98583e96e1c1daadabc80b585aa94
GitHub Action <action@github.com>
2021-08-12 15:42:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/rcbass/intellijpluginrnnconstructor/MyBundle.kt
+++ src/main/kotlin/com/github/rcbass/intellijpluginrnnconstructor/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rcbass.intellijpluginrnnconstructor
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/rcbass/intellijpluginrnnconstructor/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rcbass.intellijpluginrnnconstructor.services
+
+import com.github.rcbass.intellijpluginrnnconstructor.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
